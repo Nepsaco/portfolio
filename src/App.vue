748777677119287ae16ca19baa130fba478d66af
Nepsaco <tobie.tsuzuki@gmail.com>
2020-01-31 14:44:45 -0700

    main content area added. working on local font and scrolling description words

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,24 @@
 <template>
     <div id="app">
-        <section class='landing'>
-            <header class='main-header'>
-                <h1>Software Developer</h1>
-            </header>
-            <nav class='main-nav'>
-                <p>Hamburger</p>
-            </nav>
-            <main>
-                <h1>hello</h1>
-            </main>
-            <footer class='main-footer'>
-                <ul>
-                    <li>about</li>/
-                    <li>work</li>/
-                    <li>contact</li>
-                </ul>
-            </footer>
-        </section>
+        <Landing />
     </div>
 </template>
 
 <script>
+import Landing from '@/components/Landing.vue'
+
 export default {
+    components: {
+        Landing
+    }
 
 }
 </script>
 
 <style lang="scss">
-@import './styles/reset';
-@import './styles/typography';
-@import './styles/color';
 
 body {
     @include regular-font;
 }
-.landing {
-    display: flex;
-    height: 100vh;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    background-color: $dark;
-    color: $light;
-    padding: 2rem;
-}
-.main-header {
-    color: $accent;
-}
 
-nav {
-    position: absolute;
-    left: 2rem;
-    top: 2rem;
-    width: 8rem;
-}
-
-.main-footer {
-    color: $accent;
-    width: 100vw;
-    ul {
-        display: flex;
-        justify-content: space-evenly;
-    }
-}
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class='landing'>
+        <header class='main-header'>
+            <h1>Software Developer</h1>
+        </header>
+        <nav class='main-nav'>
+            <p>Hamburger</p>
+        </nav>
+        <main class='main-content'>
+            <h1>Tobie Tsuzuki</h1>
+            <h2>I'M A </h2>
+        </main>
+        <footer class='main-footer'>
+            <ul>
+                <li>about</li>/
+                <li>work</li>/
+                <li>contact</li>
+            </ul>
+        </footer>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style lang='scss'>
+.landing {
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background-color: $dark;
+    color: $light;
+    padding: 2rem;
+}
+
+.main-content {
+    @include header-font;
+    h2 {
+        font-size: 8vw;
+    }
+}
+
+.main-header {
+    color: $accent;
+}
+
+nav {
+    position: absolute;
+    left: 2rem;
+    top: 2rem;
+    width: 8rem;
+}
+
+.main-footer {
+    color: $accent;
+    width: 100vw;
+    ul {
+        display: flex;
+        justify-content: space-evenly;
+    }
+}
+</style>
