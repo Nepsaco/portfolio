5ebbcdc308f70066fec5364c896836e6d4fcc479
Nepsaco <tobie.tsuzuki@gmail.com>
2020-01-31 11:09:13 -0700

    main section formatting

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <header class='main-header'>
+          <h1>Hello</h1>
+      </header>
+      <nav class='main-nav'>
+          <p>Hamburger</p>
+      </nav>
+      <footer class='main-footer'>
+          <p>about</p>
+          <p>work</p>
+          <p>contact</p>
+      </footer>
   </div>
 </template>
 
+<script>
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import './styles/reset';
+@import './styles/color';
+body {
+    display: flex;
+    justify-content: center;
+    background-color: $dark;
+    align-content: space-between;
+    color: $light;
 }
 
-#nav {
-  padding: 30px;
+nav {
+    position: fixed;
+    left: 5%;
+    top: 5%;
+    width: 8em;
+    margin-top: -2.5em;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+footer {
+    position: absolute;
+    bottom: 0;
 }
 </style>
