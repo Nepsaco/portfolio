343433519c54e18d5f17e1915a522323b1eedb53
Nepsaco <tobie.tsuzuki@gmail.com>
2020-03-04 14:21:33 -0700

    added more about me content

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,10 +7,25 @@
             <h2>I am a Software Developer living in Denver, Colorado.</h2>
             <p>A former beer brewer turned engineer. I aspire to create web apps that achieve beautiful design and memorable UX.</p>
             <ul>
+                <h2>Skills</h2>
                 <li>Front-end Development</li>
                 <li>Back-end Development</li>
                 <li>UI/UX Design</li>
             </ul>
+            <ul>
+                <h2>Technolgies</h2>
+                <li>Javascript</li>
+                <li>Node</li>
+                <li>Vue</li>
+                <li>React</li>
+                <li>Ruby</li>
+                <li>Rails</li>
+            </ul>
+            <h3>
+                I am eager to learn and motivated to solve challenging problems. 
+                With my background in project management,
+                and teaching I have been helping people connect and  
+            </h3>
         </div>
     </section>
 </template>
@@ -42,13 +57,30 @@
 }
 
 .about-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
     background-color: $light;
     height: 100vh;
     padding: 2rem;
     color: $dark;
-    h2 {
+    line-height: 1.5rem;
+
+    h2, h3 {
+        padding-bottom: .5rem;
         @include bold-font;
     }
+    
+    li {
+        padding-bottom: .5rem;
+        text-indent: 1rem;
+
+    }
+
+    p {
+        padding-bottom: .5rem;
+    }
+
     @include respond-to(desktop){
         background-color: $light;
         width: 50vw
