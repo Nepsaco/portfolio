16d94ab09eae362d094d87744a2620f9eb60d06d
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2024-06-27 23:49:29 -0600

    adding chat button to start

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 <template>
-    <div id="app">
-        <Landing />
-        <About />
-        <Work />
-        <Contact />
+  <div id="app">
+    <Landing />
+    <About />
+    <Work />
+    <Contact />
+    <div class='chat-button'>
+      <button>Chat</button>
     </div>
+  </div>
 </template>
 
 <script>
-import Landing from '@/views/Landing'
-import About from '@/views/About'
-import Work from '@/views/Work'
-import Contact from '@/views/Contact'
+import Landing from "@/views/Landing";
+import About from "@/views/About";
+import Work from "@/views/Work";
+import Contact from "@/views/Contact";
 
-export default{
-
-    components: {
-        Landing,
-        About,
-        Work,
-        Contact,
-    }
-}
+export default {
+  components: {
+    Landing,
+    About,
+    Work,
+    Contact,
+  },
+};
 </script>
 
 <style lang="scss">
-
 #app {
-    @include regular-font;
+  @include regular-font;
 }
-
+.chat-button {
+    position: sticky;
+    bottom: 1rem;
+    right: 1rem;
+}
 </style>
